e814955e64e66022a32674a625ee5b9841871017
MAKSIM89PW <123>
2023-05-30 21:05:50 +0300

    AnimatedTableView

---
+++ AnimatedTableView/AnimatedTableViewCell/Controllers/TableViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Like ♥︎ Subscribe"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        30
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        cell.textLabel!.text = "Like ♥︎ Subscribe"
+        return cell
+    }
+}
