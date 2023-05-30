16a0ac5baa24224da29ece426a983011924a9bd3
MAKSIM89PW <123>
2023-05-30 21:12:46 +0300

    AnimatedTableView

--- AnimatedTableView/AnimatedTableViewCell/Controllers/TableViewController.swift
+++ AnimatedTableView/AnimatedTableViewCell/Controllers/TableViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateTableView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +23,25 @@ class TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         cell.textLabel!.text = "Like ♥︎ Subscribe"
         return cell
+    }
+    
+    private func animateTableView() {
+        tableView.reloadData()
+        
+        let cells = tableView.visibleCells
+        let tableViewHeight = tableView.bounds.height
+        var delay: Double = 0
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+            
+            UIView.animate(withDuration: 1.5,
+                           delay: delay * 0.05,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0,
+                           options: .curveEaseInOut,
+                           animations: { cell.transform = CGAffineTransform.identity })
+            delay += 1
+        }
     }
 }
